4ba5a2fc601b7afa1f49e92bb3e8c7f20e5066d2
TingedLicense <iegor1081@gmail.com>
2021-10-28 19:24:30 +0300

    Создали папки и добавили Model пользователя

---
+++ Albinaserv/Albina.DataAccessCore/Models/UserRto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Albina.DataAccessCore.Models
+{
+    [Table("User")]
+    class UserRto
+    {
+        public string  { get; set; }
+    }
+}
