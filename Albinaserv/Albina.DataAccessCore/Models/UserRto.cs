751b014f6b01e651fd9b01642651381f89f70ef5
TingedLicense <iegor1081@gmail.com>
2021-11-09 19:25:39 +0300

    Rto

--- Albinaserv/Albina.DataAccessCore/Models/UserRto.cs
+++ Albinaserv/Albina.DataAccessCore/Models/UserRto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,12 @@ namespace Albina.DataAccessCore.Models
     [Table("User")]
     class UserRto
     {
-        public string  { get; set; }
+        [Key] public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [Required] public string PhoneNumberPrefix { get; set; }
+        [Required] public string PhoneNumber { get; set; }
+        [Required, MinLenght(7)]public string Password { get; set; }
+        public string ImageName { get; set; }
     }
 }
